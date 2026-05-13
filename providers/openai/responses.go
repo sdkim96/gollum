4461af5fcef94af795ef33f303119b581d8ef34f
sdkim96 <seong960603@gmail.com>
2026-05-14 08:56:59 +0900

    added openai responses api

--- providers/openai/responses.go
+++ providers/openai/responses.go
@@ -1,11 +1,36 @@
 package openai
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type ResponsesService struct {
 	c *Client
 }
 
 func (s *ResponsesService) Create(ctx context.Context, params *ResponsesParams) (*ResponsesResponse, error) {
-	return &ResponsesResponse{}, nil
+	data, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+	rawReq, err := http.NewRequestWithContext(ctx, "POST", fmt.Sprintf("%s/responses", s.c.BaseURL.Host), bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	rawResp, err := s.c.client.Do(rawReq)
+	if err != nil {
+		return nil, err
+	}
+	defer rawResp.Body.Close()
+
+	var resp ResponsesResponse
+	err = json.NewDecoder(rawResp.Body).Decode(&resp)
+	if err != nil {
+		return nil, err
+	}
+	return &resp, nil
 }
